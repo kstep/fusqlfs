24987511628579ab933b9fb1dd4dc7116ace3686
Konstantin Stepanov <kstep@p-nut.info>
2010-02-25 04:40:09 +0200

    separated Base class for database abstraction layer

---
+++ FusqlFS/Base.pm
@@ -0,0 +1,228 @@
+package FusqlFS::Base;
+
+use strict;
+
+use DBI;
+use POSIX qw(mktime);
+
+sub new {
+    my $class = shift;
+    my $self = {
+	'table_info_cache' => {},
+	'index_info_cache' => {},
+    };
+    bless $self, $class;
+    return $self;
+}
+
+sub init {
+    my $self = shift;
+    my $options = shift;
+
+    $self->{'dbh'} = DBI->connect($self->{'dsn'}, $options->{'user'}, $options->{'password'});
+}
+
+sub DESTROY {
+    $_[0]->{'dbh'}->disconnect();
+}
+
+# Table operations {{{
+
+# @abstract
+sub get_table_list {}
+
+##
+# Get table statistics
+# @param string table
+# @return hashref
+# @abstract
+sub get_table_stat {}
+
+# @abstract
+sub get_table_info {}
+
+##
+# Get create table clause
+# @param string table
+# @return string
+# @abstract
+sub get_create_table {}
+
+# @abstract
+sub create_table {}
+
+sub drop_table {
+    my $self = shift;
+    if ($self->{'dbh'}->do("DROP TABLE $_[0]"))
+    {
+        $self->flush_table_cache($_[0]);
+        return 1;
+    }
+    return 0;
+}
+
+sub rename_table {
+    my $self = shift;
+    $self->{'dbh'}->do("ALTER TABLE $_[0] RENAME TO $_[1]");
+}
+
+# }}}
+
+# Index operations {{{
+
+# @abstract
+sub get_index_info {}
+
+##
+# Get fields included into primary key
+# @param table
+# @return list
+# @abstract
+sub get_primary_key {}
+
+# @abstract
+sub create_index {}
+
+# @abstract
+sub drop_index {}
+
+sub modify_index {
+    my $self = shift;
+    my ($table, $index, $indexinfo) = @_;
+    $self->drop_index($table, $index);
+    $self->create_index($table, $index, $indexinfo);
+}
+
+# }}}
+
+# Fields operations {{{
+
+sub create_field {
+    my $self = shift;
+    if ($self->{'dbh'}->do("ALTER TABLE $_[0] ADD $_[1] int NOT NULL DEFAULT 0"))
+    {
+        $self->flush_table_cache($_[0]);
+        return 1;
+    }
+    return 0;
+}
+
+# @abstract
+sub modify_field {}
+
+# @abstract
+sub change_field {}
+
+##
+# Remove field from table
+# @param string table
+# @param string field
+# @return bool
+sub drop_field {
+    my $self = shift;
+    if ($self->{'dbh'}->do("ALTER TABLE $_[0] DROP $_[1]"))
+    {
+        $self->flush_table_cache($_[0]);
+        return 1;
+    }
+    return 0;
+}
+
+# }}}
+
+# Data operations {{{
+
+# @abstract
+sub get_table_data {}
+
+###
+# Get record from database
+# @param string table
+# @param hashref condition
+# @param bool full
+# @return list|hashref
+sub get_record {
+    my $self = shift;
+    my ($table, $condition, $full) = @_;
+    my @keys = keys %$condition;
+    my $sql = "SELECT ". ($full? "*": join(',', @keys));
+    $sql .= " FROM $table WHERE ". join(' AND ', map { "$_ = ?" } @keys);
+    return wantarray? $self->{'dbh'}->selectrow_array($sql, undef, values %$condition): $self->{'dbh'}->selectrow_hashref($sql, undef, values %$condition);
+}
+
+sub insert_record {
+    my $self = shift;
+    my ($table, $record) = @_;
+    my $sql = "INSERT INTO $table (";
+    $sql .= join(',', keys %$record);
+    $sql .= ") VALUES (". substr(',?' x scalar keys %$record, 1) .")";
+    return $self->{'dbh'}->do($sql, undef, values %$record);
+}
+
+sub update_record {
+    my $self = shift;
+    my ($table, $condition, $record) = @_;
+    my $sql = "UPDATE $table SET ";
+    my @values = (values %$record, values %$condition);
+    $sql .= join(',', map { "$_ = ?" } keys %$record);
+    $sql .= " WHERE ". join(' AND ', map { "$_ = ?" } keys %$condition);
+    return $self->{'dbh'}->do($sql, undef, @values);
+}
+
+sub save_record {
+    my $self = shift;
+    my ($table, $condition, $record) = @_;
+    my $crecord = $self->get_record($table, $condition);
+    if ($crecord) {
+        $self->update_record($table, $condition, $record);
+    } else {
+        $self->insert_record($table, $record);
+    }
+}
+
+sub delete_record {
+    my $self = shift;
+    my ($table, $record) = @_;
+    my $sql = "DELETE FROM $table WHERE ";
+    $sql .= join(' AND ', map { "$_ = ?" } keys %$record);
+    return $self->{'dbh'}->do($sql, undef, values %$record);
+}
+
+# }}}
+
+# Utility functions {{{
+
+# @abstract
+# @static
+sub convert_field_to_sql {}
+
+# @static
+sub parse_sql_time {
+    my $time = shift;
+    my $result = 0;
+    #              1 year  2 month 3 day   4 hour  5 min   6 sec
+    if ($time =~ /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/) {
+        $result = mktime($6, $5, $4, $3, $2 - 1, $1 - 1900);
+    }
+    return $result;
+}
+
+sub flush_table_cache {
+    my $self = shift;
+    delete $self->{'table_info_cache'}->{$_[0]};
+    delete $self->{'index_info_cache'}->{$_[0]};
+}
+
+sub execute_queries {
+    my $self = shift;
+    my $buffer = shift;
+    foreach (split /;\n/, $$buffer) {
+        s/^\s+//; s/\s+$//;
+        next unless $_;
+        $self->{'dbh'}->do($_);
+    }
+}
+
+# }}}
+
+1;
--- FusqlFS/MySQL.pm
+++ FusqlFS/MySQL.pm
@@ -5,23 +5,20 @@ use strict;
 use DBI;
 use POSIX qw(mktime);
 
-our $dbh;
 our $def_engine;
 our $def_charset;
 our $fn_sep;
 
-our %table_info_cache = ();
-our %index_info_cache = ();
+our @ISA;
+require FusqlFS::Base;
+@ISA = ('FusqlFS::Base');
 
 sub new {
-    my $class = shift;
-    my $self = {
-        'base_rtxtsz' => {},
-        'base_itxtsz' => { qw(decimal 95 set 63 enum 63 float 46 text 46) },
-        'base_stxtsz' => 234,
-        'def_base_itxtsz' => 55,
-    };
-    bless $self, $class;
+    my $self = FusqlFS::Base::new($_[0]);
+    $self->{'base_rtxtsz'} = {};
+    $self->{'base_itxtsz'} = { qw(decimal 95 set 63 enum 63 float 46 text 46) };
+    $self->{'base_stxtsz'} = 234;
+    $self->{'def_base_itxtsz'} = 55;
     return $self;
 }
 
@@ -32,29 +29,25 @@ sub init {
     my $dsn = "DBI:mysql:database=$options->{database}";
     $dsn .= ";host=$options->{host}" if ($options->{'host'});
     $dsn .= ";port=$options->{port}" if ($options->{'port'});
-    $dbh = DBI->connect($dsn, $options->{'user'}, $options->{'password'});
+    $self->{'dsn'} = $dsn;
+    $self->SUPER::init($options);
 
     if ($options->{'charset'}) {
         $def_charset = $options->{'charset'};
-        $dbh->do("SET character_set_results = $def_charset");
-        $dbh->do("SET character_set_client = $def_charset");
-        $dbh->do("SET character_set_connection = $def_charset");
+        $self->{'dbh'}->do("SET character_set_results = $def_charset");
+        $self->{'dbh'}->do("SET character_set_client = $def_charset");
+        $self->{'dbh'}->do("SET character_set_connection = $def_charset");
     }
 
     $def_engine = $options->{'useinnodb'}? 'InnoDB': 'MyISAM';
     $fn_sep = $options->{'fnsep'} || '.';
 }
 
-sub DESTROY {
-    my $self = shift;
-    $dbh->disconnect();
-}
-
 # Table operations {{{
 
 sub get_table_list {
     my $self = shift;
-    my $result = $dbh->selectcol_arrayref("SHOW TABLES") || [];
+    my $result = $self->{'dbh'}->selectcol_arrayref("SHOW TABLES") || [];
     return @$result;
 }
 
@@ -67,7 +60,7 @@ sub get_table_stat {
     my $table = shift;
 
     my %result;
-    my $sth = $dbh->prepare("SHOW TABLE STATUS".(defined $table && " LIKE '$table'"));
+    my $sth = $self->{'dbh'}->prepare("SHOW TABLE STATUS".(defined $table && " LIKE '$table'"));
 
     $sth->execute();
     while (my @row = $sth->fetchrow_array()) {
@@ -82,9 +75,9 @@ sub get_table_stat {
             'Index_length'    => 0 + $row[8],
             'Data_free'       => 0 + $row[9],
             'Auto_increment'  => 0 + $row[10],
-            'Create_time'     => parse_sql_time($row[11]),
-            'Update_time'     => parse_sql_time($row[12]),
-            'Check_time'      => parse_sql_time($row[13]),
+            'Create_time'     => FusqlFS::Base::parse_sql_time($row[11]),
+            'Update_time'     => FusqlFS::Base::parse_sql_time($row[12]),
+            'Check_time'      => FusqlFS::Base::parse_sql_time($row[13]),
             'Collation'       => $row[14],
             'Checksum'        => $row[15],
             'Create_options'  => $row[16],
@@ -100,17 +93,17 @@ sub get_table_info {
     my $self = shift;
     my ($table, $field) = @_;
 
-    if (exists $table_info_cache{$table} && %{ $table_info_cache{$table} }) {
+    if (exists $self->{'table_info_cache'}->{$table} && %{ $self->{'table_info_cache'}->{$table} }) {
 
         if (wantarray) {
-            return $field? exists $table_info_cache{$table}->{$field}?($field):(): keys %{ $table_info_cache{$table} };
+            return $field? exists $self->{'table_info_cache'}->{$table}->{$field}?($field):(): keys %{ $self->{'table_info_cache'}->{$table} };
         } else {
-            return $field? $table_info_cache{$table}->{$field}:
-            $table_info_cache{$table};
+            return $field? $self->{'table_info_cache'}->{$table}->{$field}:
+            $self->{'table_info_cache'}->{$table};
         }
     }
 
-    my $sth = $dbh->prepare("SHOW FULL COLUMNS FROM $table");
+    my $sth = $self->{'dbh'}->prepare("SHOW FULL COLUMNS FROM $table");
 
     my %result;
 
@@ -144,7 +137,7 @@ sub get_table_info {
     }
     $sth->finish();
 
-    $table_info_cache{$table} = \%result unless $field;
+    $self->{'table_info_cache'}->{$table} = \%result unless $field;
     return wantarray? keys %result: ($field? $result{$field}: \%result);
 }
 
@@ -154,29 +147,15 @@ sub get_table_info {
 # @return string
 sub get_create_table {
     my $self = shift;
-    my @row = $dbh->selectrow_array("SHOW CREATE TABLE $_[0]");
+    my @row = $self->{'dbh'}->selectrow_array("SHOW CREATE TABLE $_[0]");
     return @row? $row[1]: undef;
 }
 
 sub create_table {
     my $self = shift;
-    return $dbh->do("CREATE TABLE $_[0] ($_[1] int NOT NULL auto_increment, PRIMARY KEY (id))".($def_engine && " ENGINE=$def_engine").($def_charset && " DEFAULT CHARSET=$def_charset"));
+    return $self->{'dbh'}->do("CREATE TABLE $_[0] ($_[1] int NOT NULL auto_increment, PRIMARY KEY (id))".($def_engine && " ENGINE=$def_engine").($def_charset && " DEFAULT CHARSET=$def_charset"));
 }
 
-sub drop_table {
-    my $self = shift;
-    if ($dbh->do("DROP TABLE $_[0]"))
-    {
-        $self->flush_table_cache($_[0]);
-        return 1;
-    }
-    return 0;
-}
-
-sub rename_table {
-    my $self = shift;
-    $dbh->do("ALTER TABLE $_[0] RENAME TO $_[1]");
-}
 # }}}
 
 # Index operations {{{
@@ -185,16 +164,16 @@ sub get_index_info {
     my $self = shift;
     my ($table, $index) = @_;
 
-    if (exists $index_info_cache{$table}->{$index}) {
+    if (exists $self->{'index_info_cache'}->{$table}->{$index}) {
         if (wantarray) {
-            return $index? @{ $index_info_cache{$table}->{$index}->{'Column_name'} }: keys %{ $index_info_cache{$table} };
+            return $index? @{ $self->{'index_info_cache'}->{$table}->{$index}->{'Column_name'} }: keys %{ $self->{'index_info_cache'}->{$table} };
         } else {
-            return $index? $index_info_cache{$table}->{$index}: $index_info_cache{$table};
+            return $index? $self->{'index_info_cache'}->{$table}->{$index}: $self->{'index_info_cache'}->{$table};
         }
     }
 
     my %result;
-    my $sth = $dbh->prepare("SHOW INDEX FROM $table");
+    my $sth = $self->{'dbh'}->prepare("SHOW INDEX FROM $table");
 
     $sth->execute();
     while (my @row = $sth->fetchrow_array()) {
@@ -216,7 +195,7 @@ sub get_index_info {
     }
     $sth->finish();
 
-    $index_info_cache{$table} = \%result unless $index;
+    $self->{'index_info_cache'}->{$table} = \%result unless $index;
 
     return wantarray? ($index? @{ $result{$index}->{'Column_name'} || [] }: keys %result): ($index? $result{$index}: \%result);
 }
@@ -240,26 +219,19 @@ sub create_index {
     my $sql = "ALTER TABLE $table ADD $index (";
     $sql .= join(',', map { my ($name, $part) = split /$fn_sep/, $_; $part += 0; $part? "$name($part)": $name } @fields);
     $sql .= ")";
-    return $dbh->do($sql);
+    return $self->{'dbh'}->do($sql);
 }
 
 sub drop_index {
     my $self = shift;
     my ($table, $index) = @_;
     my $index = $index =~ /^PRI/? 'PRIMARY KEY': "KEY $index";
-    if ($dbh->do("ALTER TABLE $table DROP $index"))
+    if ($self->{'dbh'}->do("ALTER TABLE $table DROP $index"))
     {
-        delete $index_info_cache{$table}->{$index};
+        delete $self->{'index_info_cache'}->{$table}->{$index};
         return 1;
     }
     return 0;
-}
-
-sub modify_index {
-    my $self = shift;
-    my ($table, $index, $indexinfo) = @_;
-    $self->drop_index($table, $index);
-    $self->create_index($table, $index, $indexinfo);
 }
 
 # }}}
@@ -271,83 +243,20 @@ sub get_table_data {
     my $table = shift;
     my @keys = $self->get_primary_key($table);
     return () unless @keys;
-    my $result = $dbh->selectcol_arrayref("SELECT CONCAT_WS('$fn_sep',".join(',',@keys).") FROM $table") || [];
+    my $result = $self->{'dbh'}->selectcol_arrayref("SELECT CONCAT_WS('$fn_sep',".join(',',@keys).") FROM $table") || [];
     return @$result;
-}
-
-###
-# Get record from database
-# @param string table
-# @param hashref condition
-# @param bool full
-# @return list|hashref
-sub get_record {
-    my $self = shift;
-    my ($table, $condition, $full) = @_;
-    my @keys = keys %$condition;
-    my $sql = "SELECT ". ($full? "*": join(',', @keys));
-    $sql .= " FROM $table WHERE ". join(' AND ', map { "$_ = ?" } @keys);
-    return wantarray? $dbh->selectrow_array($sql, undef, values %$condition): $dbh->selectrow_hashref($sql, undef, values %$condition);
-}
-
-sub insert_record {
-    my $self = shift;
-    my ($table, $record) = @_;
-    my $sql = "INSERT INTO $table (";
-    $sql .= join(',', keys %$record);
-    $sql .= ") VALUES (". substr(',?' x scalar keys %$record, 1) .")";
-    return $dbh->do($sql, undef, values %$record);
-}
-
-sub update_record {
-    my $self = shift;
-    my ($table, $condition, $record) = @_;
-    my $sql = "UPDATE $table SET ";
-    my @values = (values %$record, values %$condition);
-    $sql .= join(',', map { "$_ = ?" } keys %$record);
-    $sql .= " WHERE ". join(' AND ', map { "$_ = ?" } keys %$condition);
-    return $dbh->do($sql, undef, @values);
-}
-
-sub save_record {
-    my $self = shift;
-    my ($table, $condition, $record) = @_;
-    my $crecord = $self->get_record($table, $condition);
-    if ($crecord) {
-        $self->update_record($table, $condition, $record);
-    } else {
-        $self->insert_record($table, $record);
-    }
-}
-
-sub delete_record {
-    my $self = shift;
-    my ($table, $record) = @_;
-    my $sql = "DELETE FROM $table WHERE ";
-    $sql .= join(' AND ', map { "$_ = ?" } keys %$record);
-    return $dbh->do($sql, undef, values %$record);
 }
 
 # }}}
 
 # Fields operations {{{
 
-sub create_field {
-    my $self = shift;
-    if ($dbh->do("ALTER TABLE $_[0] ADD $_[1] int NOT NULL DEFAULT 0"))
-    {
-        $self->flush_table_cache($_[0]);
-        return 1;
-    }
-    return 0;
-}
-
 sub modify_field {
     my $self = shift;
     my ($table, $field, $fdesc) = @_;
     my ($sql, @values) = convert_field_to_sql($fdesc);
     print STDERR "ALTER TABLE $table MODIFY $field $sql\n";
-    return $dbh->do("ALTER TABLE $table MODIFY $field $sql", undef, @values);
+    return $self->{'dbh'}->do("ALTER TABLE $table MODIFY $field $sql", undef, @values);
 }
 
 sub change_field {
@@ -356,37 +265,12 @@ sub change_field {
     $fdesc ||= $self->get_table_info($table, $field);
     my ($sql, @values) = convert_field_to_sql($fdesc);
     print STDERR "ALTER TABLE $table CHANGE $field $nfield $sql\n";
-    return $dbh->do("ALTER TABLE $table CHANGE $field $nfield $sql", undef, @values);
-}
-
-##
-# Remove field from table
-# @param string table
-# @param string field
-# @return bool
-sub drop_field {
-    my $self = shift;
-    if ($dbh->do("ALTER TABLE $_[0] DROP $_[1]"))
-    {
-        $self->flush_table_cache($_[0]);
-        return 1;
-    }
-    return 0;
+    return $self->{'dbh'}->do("ALTER TABLE $table CHANGE $field $nfield $sql", undef, @values);
 }
 
 # }}}
 
 # Utility functions {{{
-
-sub parse_sql_time {
-    my $time = shift;
-    my $result = 0;
-    #              1 year  2 month 3 day   4 hour  5 min   6 sec
-    if ($time =~ /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/) {
-        $result = mktime($6, $5, $4, $3, $2 - 1, $1 - 1900);
-    }
-    return $result;
-}
 
 sub convert_field_to_sql {
     my $fdesc = shift;
@@ -410,22 +294,6 @@ sub convert_field_to_sql {
     }
     $sql .= " $fdesc->{Extra}" if $fdesc->{'Extra'};
     return ($sql, @values);
-}
-
-sub flush_table_cache {
-    my $self = shift;
-    delete $table_info_cache{$_[0]};
-    delete $index_info_cache{$_[0]};
-}
-
-sub execute_queries {
-    my $self = shift;
-    my $buffer = shift;
-    foreach (split /;\n/, $$buffer) {
-        s/^\s+//; s/\s+$//;
-        next unless $_;
-        $dbh->do($_);
-    }
 }
 
 # }}}
