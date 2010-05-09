d249a88217f700b95272a876aca3e18ca99dcd56
Konstantin Stepanov <kstep@p-nut.info>
2010-05-09 17:49:34 +0300

    prototype: implemented map_path_to_obj($path) function
    input:
        string $path
    output:
        mixed $entry, Interface $pkg, @names
        $entry — leaf entry, object, defined by last path element,
        $pkg — last meet Interface package instance, met on path,
        @names — array of strings, names, met on path, to pass to Interface methods
            (e.g. $pkg->get(@names)).

--- Proto/test.pl
+++ Proto/test.pl
@@ -4,21 +4,61 @@ use Data::Dump qw(dump);
 use strict;
 use feature ':5.10';
 
-my $fs = new FsDescr('DBI:Pg:database=unite_dev', 'unite_dev', 'GtIXQeugXO4I');
+our $fs = new FsDescr('DBI:Pg:database=unite_dev', 'unite_dev', 'GtIXQeugXO4I');
 
-my $ind = $fs->{subpackages}->{tables}->{subpackages}->{indices}->list('profiles');
+sub map_path_to_obj
+{
+    my $path = shift;
+    my $pkg = $fs;
+    my $pkglvl = 0;
+    my $entry = $pkg->{subpackages};
+    my @names = ();
+
+    $path =~ s{^/}{};
+    $path =~ s{/$}{};
+    my @path = split /\//, $path;
+    foreach my $p (@path)
+    {
+        if (exists $entry->{$p})
+        {
+            $entry = $entry->{$p};
+            if (UNIVERSAL::isa($entry, 'Interface'))
+            {
+                $pkglvl++;
+                $pkg = $entry;
+                $entry = $entry->{subpackages} if exists $entry->{subpackages};
+            }
+        }
+        else
+        {
+            push @names, $p;
+        }
+    }
+    if ($pkg == $entry)
+    {
+        $entry = $pkglvl == scalar(@names)? $entry->get(@names): $entry->list(@names);
+    }
+    return wantarray? ($entry, $pkg, @names): $entry;
+}
+
+# /tables/profiles/indices
+my $entry = map_path_to_obj('/tables/profiles/indices');
+say dump $entry;
+
+# /tables/profiles/indices/ddddd
+$entry = map_path_to_obj('/tables/profiles/indices/ddddd');
+say dump $entry;
+
+# /tables/profiles/struct
+my $ind = map_path_to_obj('/tables/profiles/struct');
 say dump $ind;
 
-my $ind = $fs->{subpackages}->{tables}->{subpackages}->{indices}->get('profiles', 'ddddd');
-say dump $ind;
-
-my $ind = $fs->{subpackages}->{tables}->{subpackages}->{struct}->list('profiles');
-say dump $ind;
-
-my $ind = $fs->{subpackages}->{tables}->{subpackages}->{struct}->get('profiles', 'id');
+# /tables/profiles/struct/id
+my $ind = map_path_to_obj('/tables/profiles/struct/id');
 say $ind;
 
-my $t = $fs->{subpackages}->{tables};
+# /tables
+my ($_, $t) = map_path_to_obj('/tables');
 $t->create('testtablecreate');
 $ind = $t->list();
 say dump $ind;
@@ -26,7 +66,7 @@ $t->drop('testtablecreate');
 $ind = $t->list();
 say dump $ind;
 
-my $ind = $t->{subpackages}->{data}->list('profiles');
+my $ind = map_path_to_obj('/tables/profiles/data');
 say dump $ind;
-$ind = $t->{subpackages}->{data}->get('profiles', '22223');
-say $ind;
+my $entry = map_path_to_obj('/tables/profiles/data/22223');
+say $entry;
