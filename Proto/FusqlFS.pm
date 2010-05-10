b623cadad4f5d35d66aa4057150dff43b9daab53
Konstantin Stepanov <kstep@p-nut.info>
2010-05-10 17:24:11 +0300

    prototype: implemented basic fusefs interface

---
+++ Proto/FusqlFS.pm
@@ -0,0 +1,103 @@
+use v5.10.0;
+use strict;
+
+package FusqlFS;
+use FsDescr;
+use POSIX qw(:fcntl_h :errno_h mktime);
+use Fcntl qw(:mode);
+use Data::Dump qw(dump);
+use Fuse;
+
+our $fusqlh;
+our $def_time;
+
+sub dbg
+{
+    my $caller = caller();
+    local $, = ",";
+    print STDERR $caller, @_, "\n";
+}
+
+sub init
+{
+    $fusqlh = new FsDescr('DBI:Pg:database=unite_dev', 'unite_dev', 'GtIXQeugXO4I');
+    $def_time = mktime(localtime());
+}
+
+sub mount
+{
+    my $mountpoint = shift;
+    my %options = @_;
+
+    Fuse::main(
+        mountpoint => $mountpoint,
+        mountopts  => $options{'allow_other'}? 'allow_other': '',
+        debug      => $options{'debug'} || 0,
+
+        getdir     => \&getdir,
+        getattr    => \&getattr,
+        readlink   => \&readlink,
+    );
+}
+
+sub getdir
+{
+    
+    my ($path) = @_;
+    my $entry = $fusqlh->by_path($path);
+    return ('.', '..', @{$entry->list()}, 0);
+}
+
+sub getattr
+{
+    my ($path) = @_;
+    my $entry = $fusqlh->by_path($path);
+    dbg('getattr', $path, $entry->isdir());
+    return file_struct($entry);
+}
+
+sub readlink
+{
+    my ($path) = @_;
+    my $entry = $fusqlh->by_path($path);
+    return ${$entry->get()};
+}
+
+sub file_struct
+{
+    my ($entry) = @_;
+    my @fileinfo = (
+        undef,     # 0 dev
+        undef,     # 1 ino
+        0644,      # 2 mode
+        1,         # 3 nlink
+        $>,        # 4 uid
+        $),        # 5 gid
+        undef,     # 6 rdev
+        0,         # 7 size
+        $def_time, # 8 atime
+        $def_time, # 9 mtime
+        $def_time, # 10 ctime
+        512,       # 11 blksize
+        undef,     # 12 blocks
+    );
+    if ($entry->isdir())
+    {
+        $fileinfo[2] |= (S_IFDIR|0111);
+        $fileinfo[3] = 2;
+    }
+    elsif ($entry->islink())
+    {
+        $fileinfo[2] |= S_IFLNK;
+        $fileinfo[7] = 0 + length(${$entry->get()});
+    }
+    else
+    {
+        $fileinfo[2] |= S_IFREG;
+        $fileinfo[7] = 0 + length($entry->get());
+    }
+
+    return @fileinfo;
+}
+
+1;
---
+++ Proto/mount.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use v5.10.0;
+use strict;
+use FusqlFS;
+
+FusqlFS::init();
+FusqlFS::mount('./mnt');
