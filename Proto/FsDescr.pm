9b1efd10909b4e1a1905b9b2360da14cd4d7a8ae
Konstantin Stepanov <kstep@p-nut.info>
2010-05-10 17:23:12 +0300

    proto: rewrote mapping algorithm

--- Proto/FsDescr.pm
+++ Proto/FsDescr.pm
@@ -1,8 +1,86 @@
+use v5.10.0;
+use strict;
+
+use Data::Dump qw(dump);
+$, = ',';
+
+package Entry;
+
+sub new
+{
+    my $class = shift;
+    my $fs = shift;
+    my $path = shift;
+
+    $path =~ s{^/}{};
+    $path =~ s{/$}{};
+    my @path = split /\//, $path;
+
+    my $entry = $fs;
+    my $pkg = $entry;
+    my @names = ();
+    foreach my $p (@path)
+    {
+        return unless UNIVERSAL::isa($entry, 'HASH');
+        if (UNIVERSAL::isa($entry, 'Interface'))
+        {
+            $pkg = $entry;
+            $entry = $pkg->get(@names, $p);
+            if ($entry)
+            {
+                push @names, $p;
+            }
+            else
+            {
+                $entry = $pkg->{subpackages}->{$p} || undef;
+            }
+        }
+        else
+        {
+            $entry = $entry->{$p};
+        }
+    }
+
+    my $list;
+    if (UNIVERSAL::isa($entry, 'Interface'))
+    {
+        $pkg = $entry;
+        $list = $pkg->list(@names);
+        unless ($list)
+        {
+            $list = [ keys %{$pkg->{subpackages}} ] if exists $pkg->{subpackages};
+        }
+    }
+    elsif (my $ref = ref $entry)
+    {
+        given ($ref)
+        {
+            when ('HASH')  { $list = [ keys %$entry ] }
+            when ('ARRAY') { $list = $entry }
+            #when ('SCALAR') {}
+        }
+    }
+    my $self = [ $pkg, \@names, $entry, $list ];
+    bless $self, $class;
+}
+
+sub get { $_[0]->[2] }
+sub list { $_[0]->[3] }
+sub rename { $_[0]->[0]->rename(@{$_[0]->[1]}, @_) }
+sub drop { $_[0]->[0]->drop(@{$_[0]->[1]}) }
+sub create { $_[0]->[0]->create(@{$_[0]->[1]}) }
+sub store { $_[0]->[0]->store(@{$_[0]->[1]}, @_) }
+
+sub isdir { defined $_[0]->[3] }
+sub islink { ref $_[0]->[2] eq 'SCALAR' }
+
+1;
+
 package Interface;
 
 sub new { bless {}, $_[0] }
 sub get { return '' }
-sub list { return [] }
+sub list { return }
 sub rename { return 1 }
 sub drop { return 1 }
 sub create { return 1 }
@@ -11,15 +89,10 @@ sub store { return 1 }
 1;
 
 package FsDescr;
+use base 'Interface';
 
 use DBI;
 use YAML::Tiny;
-use Data::Dump qw(dump);
-
-use strict;
-use feature ':5.10';
-
-$, = ", ";
 
 our $dumper = \&YAML::Tiny::Dump;
 our $dbh;
@@ -32,13 +105,24 @@ sub new
         subpackages => {
             tables => new Tables(),
         },
+        cache => {},
     };
 
     bless $self, $class;
 }
 
-1;
+sub by_path
+{
+    $_[0]->{cache}->{$_[1]} = new Entry($_[0], $_[1]) unless defined $_[0]->{cache}->{$_[1]};
+    return $_[0]->{cache}->{$_[1]};
+}
 
+sub clear_cache
+{
+    delete $_[0]->{cache}->{$_[1]};
+}
+
+1;
 
 package Roles;
 use base 'Interface';
@@ -81,7 +165,6 @@ sub list
 
 package Role::Owned;
 use base 'Interface';
-
 
 1;
 
@@ -389,6 +472,12 @@ sub new
     bless $self, $class;
 }
 
+sub get
+{
+    my $self = shift;
+    return $self->{subpackages};
+}
+
 sub drop
 {
     my $self = shift;
@@ -418,53 +507,3 @@ sub list
 
 1;
 
-package Entry;
-
-sub new
-{
-    my $class = shift;
-    my $fs = shift;
-    my $path = shift;
-
-    my $pkg = $fs;
-    my $pkglvl = 0;
-    my $entry = $pkg->{subpackages};
-    my @names = ();
-
-    $path =~ s{^/}{};
-    $path =~ s{/$}{};
-    my @path = split /\//, $path;
-    foreach my $p (@path)
-    {
-        if (exists $entry->{$p})
-        {
-            $entry = $entry->{$p};
-            if (UNIVERSAL::isa($entry, 'Interface'))
-            {
-                $pkglvl++;
-                $pkg = $entry;
-                $entry = $entry->{subpackages} if exists $entry->{subpackages};
-            }
-        }
-        else
-        {
-            push @names, $p;
-        }
-    }
-    if ($pkg == $entry)
-    {
-        $entry = $pkglvl == scalar(@names)? $entry->get(@names): $entry->list(@names);
-    }
-
-    my $self = [ $pkg, \@names, $entry ];
-    bless $self, $class;
-}
-
-sub get { $_[0]->[2] ||= $_[0]->[0]->get(@{$_[0]->[1]}) }
-sub list { $_[0]->[0]->list(splice(@{$_[0]->[1]}, 0, -1)) }
-sub rename { $_[0]->[0]->rename(@{$_[0]->[1]}, @_) }
-sub drop { $_[0]->[0]->drop(@{$_[0]->[1]}) }
-sub create { $_[0]->[0]->create(@{$_[0]->[1]}) }
-sub store { $_[0]->[0]->store(@{$_[0]->[1]}, @_) }
-
-1;
