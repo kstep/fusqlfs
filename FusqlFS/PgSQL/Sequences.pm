8b686c6ef7506f4d993c4dc31c1ec6a78ba49305
Konstantin Stepanov <kstep@p-nut.info>
2010-05-15 07:44:35 +0300

    pgsql sequences: rename and store support implemented

--- FusqlFS/PgSQL/Sequences.pm
+++ FusqlFS/PgSQL/Sequences.pm
@@ -4,6 +4,7 @@ use FusqlFS::Base;
 
 package FusqlFS::PgSQL::Sequences;
 use base 'FusqlFS::Base::Interface';
+use DBI qw(:sql_types);
 
 sub new
 {
@@ -13,6 +14,7 @@ sub new
     $self->{list_expr} = $class->expr("SELECT relname FROM pg_catalog.pg_class WHERE relkind = 'S'");
     $self->{exists_expr} = $class->expr("SELECT 1 FROM pg_catalog.pg_class WHERE relkind = 'S' AND relname = ?");
     $self->{get_expr} = 'SELECT * FROM "%s"';
+    $self->{rename_expr} = 'ALTER SEQUENCE "%s" RENAME TO "%s"';
 
     $self->{subpackages} = {
     };
@@ -33,6 +35,57 @@ sub list
 {
     my $self = shift;
     return $self->all_col($self->{list_expr}) || [];
+}
+
+sub store
+{
+    my $self = shift;
+    my ($name, $data) = @_;
+    $data = $self->load($data->{struct})||{};
+
+    my $sql = "ALTER SEQUENCE \"$name\" ";
+
+    my %params = (
+        increment_by => ['INCREMENT BY', SQL_INTEGER, 0],
+        cache_value  => ['CACHE', SQL_INTEGER, 0],
+        last_value   => ['RESTART WITH', SQL_INTEGER, 0],
+        max_value    => ['MAXVALUE', SQL_INTEGER, 1],
+        min_value    => ['MINVALUE', SQL_INTEGER, 1],
+    );
+
+    my @binds = ();
+    my @types = ();
+    foreach (keys %params)
+    {
+        next unless exists $data->{$_};
+        if (!defined $data->{$_})
+        {
+            $sql .= 'NO '.$params{$_}->[0].' ' if $params{$_}->[2];
+            next;
+        }
+        $sql .= $params{$_}->[0].' ? ';
+        push @binds, $data->{$_};
+        push @types, $params{$_}->[1];
+    }
+
+    if (exists $data->{is_cycled})
+    {
+        $sql .= $data->{is_cycled}? 'CYCLE ': 'NO CYCLE ';
+    }
+
+    my $sth = $self->expr($sql);
+    foreach (0..$#binds)
+    {
+        $sth->bind_param($_+1, $binds[$_], $types[$_]);
+    }
+    $sth->execute();
+}
+
+sub rename
+{
+    my $self = shift;
+    my ($name, $newname) = @_;
+    $self->do($self->{rename_expr}, [$name, $newname]);
 }
 
 1;
