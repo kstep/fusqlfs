3bdcf42df9119eae40f495f4c7ac826073c24d1a
Konstantin Stepanov <kstep@p-nut.info>
2010-05-25 02:04:26 +0300

    added inline tests for Cache::Limited module

--- lib/FusqlFS/Cache/Limited.pm
+++ lib/FusqlFS/Cache/Limited.pm
@@ -4,11 +4,73 @@ use v5.10.0;
 package FusqlFS::Cache::Limited;
 use parent 'FusqlFS::Cache::Base';
 
+=begin testing
+
+#!noinst
+
+ok FusqlFS::Cache::Limited->is_needed(10), 'Limited cache is needed';
+ok !FusqlFS::Cache::Limited->is_needed(0), 'Limited cache isn\'t needed';
+
+=end testing
+=cut
 sub is_needed
 {
     return $_[1] > 0;
 }
 
+=begin testing
+
+my %cache;
+isa_ok tie(%cache, 'FusqlFS::Cache::Limited', 10), 'FusqlFS::Cache::Limited', 'Limited cache tied';
+
+ok !scalar(%cache), 'Cache is empty';
+
+foreach my $n (1..10)
+{
+    $cache{'test'.$n} = 'value'.$n;
+}
+
+ok scalar(%cache), 'Cache is not empty';
+
+foreach my $n (1..10)
+{
+    ok exists($cache{'test'.$n}), 'Entry '.$n.' exists';
+    is $cache{'test'.$n}, 'value'.$n, 'Entry '.$n.' is intact';
+}
+
+ok exists($cache{'test10'}), 'Element exists before deletion';
+delete $cache{'test10'};
+ok !exists($cache{'test10'}), 'Deleted element doesn\'t exist';
+is $cache{'test10'}, undef, 'Deleted element is undefined';
+
+%cache = ();
+
+ok !scalar(%cache), 'Cache is empty after cleanup';
+
+foreach my $n (1..1000)
+{
+    $cache{'test'.$n} = 'value'.$n;
+    foreach my $m (1..1000-$n)
+    {
+        my $x = $cache{'test'.$n};
+    }
+}
+
+ok exists($cache{'test1'}), 'Most used element exists';
+is $cache{'test1'}, 'value1', 'Most used element is intact';
+ok !exists($cache{'test999'}), 'Least used element doesn\'t exist';
+is $cache{'test999'}, undef, 'Least used element undefined';
+cmp_ok length(%cache), '<=', 10, 'Number of items in cache doesn\'t exceed given threshold';
+
+while (my ($key, $val) = each %cache)
+{
+    like $key, qr/^test[0-9]+$/, 'Iterate: key is '.$key.' intact';
+    like $val, qr/^value[0-9]+$/, 'Iterate: value is '.$val.' intact';
+    is substr($key, 4), substr($val, 5), 'Iterate: key matches value';
+}
+
+=end testing
+=cut
 sub TIEHASH
 {
     my $class = shift;
