c04d00bf22bc1eb15b0d2f51f8d4329731d6ad0d
Konstantin Stepanov <kstep@p-nut.info>
2010-05-25 01:05:59 +0300

    added inline tests for PgSQL::Queries + implemented rename for queries

--- lib/FusqlFS/Backend/PgSQL/Queries.pm
+++ lib/FusqlFS/Backend/PgSQL/Queries.pm
@@ -11,6 +11,12 @@ sub new
     bless $self, $class;
 }
 
+=begin testing get
+
+is $_tobj->get('query'), undef;
+
+=end testing
+=cut
 sub get
 {
     my $self = shift;
@@ -18,12 +24,26 @@ sub get
     return $self->{$name}||undef;
 }
 
+=begin testing list
+
+list_ok $_tobj->list(), [];
+
+=end testing
+=cut
 sub list
 {
     my $self = shift;
     return [ keys %$self ];
 }
 
+=begin testing create after get list
+
+isnt $_tobj->create('query'), undef;
+isa_ok $_tobj->get('query'), 'CODE';
+is_deeply $_tobj->list(), [ 'query' ];
+
+=end testing
+=cut
 sub create
 {
     my $self = shift;
@@ -35,6 +55,14 @@ sub create
     };
 }
 
+=begin testing drop after rename
+
+isnt $_tobj->drop('new_query'), undef;
+is $_tobj->get('new_query'), undef;
+is_deeply $_tobj->list(), [];
+
+=end testing
+=cut
 sub drop
 {
     my $self = shift;
@@ -42,5 +70,32 @@ sub drop
     delete $self->{$name};
 }
 
+=begin testing rename after create
+
+my $oldquery = $_tobj->get('query');
+isnt $_tobj->rename('query', 'new_query'), undef;
+is $_tobj->get('query'), undef;
+is $_tobj->get('new_query'), $oldquery;
+is_deeply $_tobj->list(), [ 'new_query' ];
+
+=end testing
+=cut
+sub rename
+{
+    my $self = shift;
+    my ($name, $newname) = @_;
+    return unless exists $self->{$name};
+    $self->{$newname} = $self->{$name};
+    delete $self->{$name};
+}
+
 1;
 
+__END__
+
+=begin testing SETUP
+
+#!class FusqlFS::Backend::PgSQL::Test
+
+=end testing
+=cut
