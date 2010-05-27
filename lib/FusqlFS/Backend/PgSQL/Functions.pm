bdaec14de2b1bf78bb7ee609a1b81f8b7ee2e6f5
Konstantin Stepanov <kstep@p-nut.info>
2010-05-27 15:59:08 +0300

    PgSQL::Functions: put arguments into names

--- lib/FusqlFS/Backend/PgSQL/Functions.pm
+++ lib/FusqlFS/Backend/PgSQL/Functions.pm
@@ -9,14 +9,14 @@ sub new
     my $class = shift;
     my $self = {};
 
-    $self->{list_expr} = $class->expr('SELECT DISTINCT p.proname FROM pg_catalog.pg_proc AS p
+    $self->{list_expr} = $class->expr('SELECT DISTINCT p.proname||\'(\'||pg_catalog.pg_get_function_arguments(p.oid)||\')\' FROM pg_catalog.pg_proc AS p
                 LEFT JOIN pg_catalog.pg_namespace AS ns ON ns.oid = p.pronamespace
             WHERE ns.nspname = \'public\'');
 
     $self->{get_expr} = $class->expr('SELECT pg_catalog.pg_get_function_result(p.oid) AS result,
                 pg_catalog.pg_get_function_arguments(p.oid) AS arguments,
                 CASE WHEN p.proisagg THEN NULL ELSE pg_catalog.pg_get_functiondef(p.oid) END AS struct
-            FROM pg_catalog.pg_proc AS p WHERE p.proname = ? ORDER BY arguments, result');
+            FROM pg_catalog.pg_proc AS p WHERE p.proname = ? AND pg_catalog.pg_get_function_arguments(p.oid) = ? ORDER BY arguments, result');
 
     $self->{create_expr} = 'CREATE OR REPLACE FUNCTION %s(integer) RETURNS integer LANGUAGE sql AS $function$ SELECT $1; $function$';
 
@@ -25,23 +25,25 @@ sub new
 
 =begin testing get
 
-my $row = $_tobj->get('sort');
-isnt $row, undef, '->get() result is sane';
+my $row = $_tobj->get('xxxxx');
+is $row, undef, '->get() result is sane';
 
 =end testing
 =cut
 sub get
 {
     my $self = shift;
-    my ($name) = @_;
-    return $self->all_row($self->{get_expr}, $name);
+    my ($name, $args) = split(/\(/, $_[0], 2);
+    return unless $args;
+    $args =~ s/\)$//;
+    return $self->one_row($self->{get_expr}, $name, $args);
 }
 
 =begin testing list
 
 my $list = $_tobj->list();
 isa_ok $list, 'ARRAY', '->list() result is an array';
-cmp_ok scalar(@$list), '>', 0, '->list() result is not empty';
+cmp_ok scalar(@$list), '==', 0, '->list() result is empty';
 
 =end testing
 =cut
