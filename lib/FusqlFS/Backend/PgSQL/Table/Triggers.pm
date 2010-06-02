e11c0f260665acbaebe59bbb2837c3f50c7a5cb3
Konstantin Stepanov <kstep@p-nut.info>
2010-06-02 14:27:28 +0300

    PgSQL::Table::Triggers: other methods implemented

--- lib/FusqlFS/Backend/PgSQL/Table/Triggers.pm
+++ lib/FusqlFS/Backend/PgSQL/Table/Triggers.pm
@@ -26,7 +26,7 @@ FusqlFS::Backend::PgSQL::Table::Triggers
 =cut
 
 package FusqlFS::Backend::PgSQL::Table::Triggers;
-use parent 'FusqlFS::Artifact';
+use parent 'FusqlFS::Artifact::Table::Lazy';
 
 sub new
 {
@@ -39,9 +39,7 @@ sub new
     # 2  4 = insert
     # 3  8 = delete
     # 4 16 = update
-    # after idu stmt = 00011100
-    # after iu row   = 00010101
-    # before i stmt  = 00000110
+    # 5 32 = truncate (?)
     $self->{get_expr} = $class->expr('SELECT pg_catalog.pg_get_triggerdef(t.oid) AS "create.sql",
             p.proname||\'(\'||pg_catalog.pg_get_function_arguments(p.oid)||\')\' AS handler,
             CASE WHEN (t.tgtype & 1) != 0 THEN \'row\' ELSE \'statement\' END AS for_each,
@@ -50,6 +48,7 @@ sub new
                 CASE WHEN (t.tgtype &  4) != 0 THEN \'insert\' END,
                 CASE WHEN (t.tgtype &  8) != 0 THEN \'delete\' END,
                 CASE WHEN (t.tgtype & 16) != 0 THEN \'update\' END
+                CASE WHEN (t.tgtype & 32) != 0 THEN \'truncate\' END
                 ] AS events
         FROM pg_catalog.pg_trigger AS t
             LEFT JOIN pg_catalog.pg_class AS r ON (t.tgrelid = r.oid)
@@ -58,6 +57,20 @@ sub new
     $self->{list_expr} = $class->expr('SELECT t.tgname FROM pg_catalog.pg_trigger AS t
         LEFT JOIN pg_catalog.pg_class AS r ON (t.tgrelid = r.oid) WHERE r.relname = ?');
 
+    $self->{rename_expr} = 'ALTER TRIGGER %s ON %s RENAME TO %s';
+    $self->{drop_expr} = 'DROP TRIGGER %s ON %s';
+
+    $self->{template} = {
+        'create.sql' => '---
+events:
+    - insert
+    - update
+    - delete
+for_each: row
+when: before
+',
+    };
+
     bless $self, $class;
 }
 
@@ -65,27 +78,62 @@ sub get
 {
     my $self = shift;
     my ($table, $name) = @_;
-    my $data = $self->one_row($self->{get_expr}, $table, $name);
-    return unless $data;
 
-    my $result = {};
+    unless ($self->SUPER::get($table, $name))
+    {
+        my $data = $self->one_row($self->{get_expr}, $table, $name);
+        return unless $data;
 
-    $data->{events} = [ grep $_, @{$data->{events}} ];
+        my $result = {};
 
-    $result->{handler} = \"../../../../functions/$data->{handler}";
-    delete $data->{handler};
+        $data->{events} = [ grep $_, @{$data->{events}} ];
 
-    $result->{'create.sql'} = delete($data->{'create.sql'});
-    $result->{struct} = $self->dump($data);
+        $result->{handler} = \"../../../../functions/$data->{handler}";
+        delete $data->{handler};
 
-    return $result;
+        $result->{'create.sql'} = delete($data->{'create.sql'});
+        $result->{struct} = $self->dump($data);
+
+        return $result;
+    }
 }
 
 sub list
 {
     my $self = shift;
     my ($table) = @_;
-    return $self->all_col($self->{list_expr}, $table);
+    return [ @{$self->all_col($self->{list_expr}, $table)}, @{$self->SUPER::list($table)} ];
+}
+
+sub drop
+{
+    my $self = shift;
+    my ($table, $name) = @_;
+    $self->SUPER::drop($table, $name) or $self->do($self->{drop_expr}, [$name, $table]);
+}
+
+sub store
+{
+    my $self = shift;
+    my ($table, $name, $data) = @_;
+    my $struct = $self->load($data->{struct});
+
+    my $when     = uc($struct->{when});
+    my $for_each = uc($struct->{for_each});
+    my $handler  = ${$data->{handler}};
+    $handler =~ s#^\.\./\.\./functions/##;
+
+    local $" = ' OR ';
+    my $sql = "CREATE TRIGGER $name $when @{$struct->{events}} ON $table FOR EACH $for_each EXECUTE PROCEDURE $handler";
+    $self->drop($table, $name) and $self->do($sql);
+}
+
+sub rename
+{
+    my $self = shift;
+    my ($table, $name, $newname) = @_;
+    $self->SUPER::rename($table, $name, $newname)
+        or $self->do($self->{rename_expr}, [$name, $table, $newname]);
 }
 
 1;
