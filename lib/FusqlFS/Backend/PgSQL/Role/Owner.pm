1e704b05c1b90b0890efe160a68262583c9c646b
Konstantin Stepanov <kstep@p-nut.info>
2010-06-18 15:46:37 +0400

    PgSQL::Role::Owner: documentation

--- lib/FusqlFS/Backend/PgSQL/Role/Owner.pm
+++ lib/FusqlFS/Backend/PgSQL/Role/Owner.pm
@@ -2,20 +2,82 @@ use strict;
 use v5.10.0;
 
 package FusqlFS::Backend::PgSQL::Role::Owner;
+use parent 'FusqlFS::Artifact';
 
 =head1 NAME
 
-FusqlFS::Backend::PgSQL::Role::Owner - FusqlFS class to expose artifact's owner
+FusqlFS::Backend::PgSQL::Role::Owner - FusqlFS class to expose PostgreSQL
+artifact's owner
 
 =head1 SYNOPSIS
 
+    package FusqlFS::Backend::PgSQL::Tables;
+    use parent 'FusqlFS::Artifact';
+
+    use FusqlFS::Backend::PgSQL::Role::Owner;
+
+    sub new
+    {
+        my $class = shift;
+        my $self = {};
+
+        # initialize class
+
+        $self->{owner} = FusqlFS::Backend::PgSQL::Role::Owner->new('r');
+        bless $self, $class;
+    }
+
+    sub get
+    {
+        my $self = shift;
+        my ($name) = @_;
+        my $result = {};
+
+        # load structures into $result
+
+        $result->{owner} = $self->{owner};
+        return $result;
+    }
 
 =head1 DESCRIPTION
 
+This class exposes PostgreSQL artifact's owner as a symlink to role in
+F</roles> directory. It is best used with plugged in
+L<FusqlFS::Backend::PgSQL::Roles> module (see L<FusqlFS::Backend::Base> for
+more info on plugging in different modules).
+
+The class's C<new> constructor accepts single char argument designating
+type of artifact the owner of which is to be exposed. Possible values are:
+
+=over
+
+=item C<r>
+
+Table (a.k.a. relation).
+
+=item C<i>
+
+Table's index.
+
+=item C<S>
+
+Sequence.
+
+=item C<v>
+
+View.
+
+=item C<_F>
+
+Function (a.k.a. stored procedure).
+
+=item C<_L>
+
+Language.
+
+=back
 
 =cut
-
-use parent 'FusqlFS::Artifact';
 
 our %relkinds = (
     r  => [ qw(TABLE rel) ],
