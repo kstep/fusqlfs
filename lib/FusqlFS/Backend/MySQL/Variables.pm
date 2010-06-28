6c634be2760b03b432d296775b29bb17cc7bf7dc
Konstantin Stepanov <kstep@p-nut.info>
2010-06-28 19:16:55 +0300

    MySQL: stub backend modules added

---
+++ lib/FusqlFS/Backend/MySQL/Variables.pm
@@ -0,0 +1,19 @@
+use strict;
+use 5.010;
+
+package FusqlFS::Backend::MySQL::Variables;
+our $VERSION = "0.005";
+use parent 'FusqlFS::Artifact';
+
+=head1 NAME
+
+FusqlFS::Backend::MySQL::Variables - 
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 EXPOSED STRUCTURE
+
+=cut
+
